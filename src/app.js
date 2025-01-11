2245a56a9ea1f1b6ec98e48b2dfffbc8944c2b4a
Dhananjana Nadee Kumari <dhananjananadeekumari@gmail.com>
2025-01-11 19:10:22 +0530

    cors enable

--- src/app.js
+++ src/app.js
@@ -3,10 +3,21 @@ import user_routers from './routers/user_controller.js';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import exp from 'constants';
+import cors from 'cors'
 
 const port = 4000;
 
 var app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:3000', // Your React app's URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Allow credentials (cookies, authorization headers, etc)
+    optionsSuccessStatus: 200
+  };
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
